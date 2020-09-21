44470687caccca7ba65e8701d0af55654e90c920
alievako <konul.aliyeva.1995@gmail.com>
2020-09-22 00:30:49 +0400

    minor changes in the source code

--- myfile.c
+++ myfile.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int i;
-    printf("this program has %d arguments\n", argc-1);
+    printf("this program has %d arguments\n", argc - 1);
     printf("the program name is %s.\n", argv[0]);
-    for(i=1;i<argc;i++){
-        printf("The %dth argument is %s\n", i, argv[i]);
+    for (i = 1; i < argc; i++)
+    {
+        switch (i){
+            case 1:
+                printf("The %dst argument is %s\n", i, argv[i]);
+                break;
+            case 2:
+                printf("The %dnd argument is %s\n", i, argv[i]);
+                break;
+            case 3:
+                printf("The %drd argument is %s\n", i, argv[i]);
+                break;
+            default:
+                printf("The %dth argument is %s\n", i, argv[i]);
+        }
     }
     printf("end\n");
 
